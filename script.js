9cb711abc8b0ee2055c8656dd78315af07152089
Vishal <vishaldutt157@gmail.com>
2021-05-15 01:16:15 +0530

    V3-Cell-Selection

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ for (let i = 1; i <= 100; i++) {
 }
 
 // Add Cells to the cell div
-for(let i=1;i<=100;i++){
+for (let i = 1; i <= 100; i++) {
     let row = $(`<div class="cell-row"></div>`)
-    for(let j=1;j<=100;j++){
-        row.append(`<div id="row-${i}-col${j}" class="input-cell" contenteditable="false"></div>`);
+    for (let j = 1; j <= 100; j++) {
+        row.append(`<div id="row-${i}-col-${j}" class="input-cell" contenteditable="false"></div>`);
     }
     $("#cells").append(row);
 }
@@ -34,24 +34,118 @@ for(let i=1;i<=100;i++){
 // columns scroll along with cell cell when we scroll cell
 // .scroll is event it shows how manu time you have scroll
 // $("#cells").scroll(function(e){
-    // console.log("hello");
-    // Left scroll
-    // console.log(this.scrollLeft);
-    // Scroll top
-    // console.log(this.scrollTop);
-    // $("#columns").scrollLeft(this.scrollLeft);
-    // $("#rows").scrollTop(this.scrollTop);
-    // this.scrollLeft and this.scrollTop states tha how scroll moves in x-axis and y-axis direction
+// console.log("hello");
+// Left scroll
+// console.log(this.scrollLeft);
+// Scroll top
+// console.log(this.scrollTop);
+// $("#columns").scrollLeft(this.scrollLeft);
+// $("#rows").scrollTop(this.scrollTop);
+// this.scrollLeft and this.scrollTop states tha how scroll moves in x-axis and y-axis direction
 // });
 
-$("#cells").scroll(function(e) {
+$("#cells").scroll(function (e) {
     $("#columns").scrollLeft(this.scrollLeft);
     $("#rows").scrollTop(this.scrollTop);
 });
 
 // Function when user double click then the cell became contenteditable
 
-$(".input-cell").dblclick(function(e) {
+$(".input-cell").dblclick(function (e) {
     $(this).attr("contenteditable", "true");
     $(this).focus();
 });
+
+$(".input-cell").blur(function (e) {
+    $(this).attr("contenteditable", "false");
+});
+
+// Click to select cell
+// $(".input-cell").click(function(e){
+//     $(".input-cell.selected").removeClass("selected");
+//     $(this).addClass("selected");
+// });
+// $(".input-cell").click(function(e){
+//     $(".input-cell.selected").removeClass("selected");
+//     $(this).addClass("selected");
+// });
+
+// function to get row and column of the selected element
+function getRowCol(ele) {
+    let id = $(ele).attr("id");
+    let idArray = id.split("-");
+    let rowId = parseInt(idArray[1]);
+    let colId = parseInt(idArray[3]);
+    return [rowId, colId];
+}
+
+// fucntion to get top left bottom and right of the element
+function getTopLeftBottomRightCell(rowId, colId) {
+    let topCell = $(`#row-${rowId - 1}-col-${colId}`);
+    let bottomCell = $(`#row-${rowId + 1}-col-${colId}`);
+    let leftCell = $(`#row-${rowId}-col-${colId - 1}`);
+    let rightCell = $(`#row-${rowId}-col-${colId + 1}`);
+    return [topCell, bottomCell, leftCell, rightCell];
+}
+
+// function to select the cells
+$(".input-cell").click(function (e) {
+    let [rowId, colId] = getRowCol(this);
+    let [topCell, bottomCell, leftCell, rightCell] = getTopLeftBottomRightCell(rowId, colId);
+    selectCell(this, e, topCell, bottomCell, leftCell, rightCell);
+});
+
+// Top selected
+// ele means current element
+function selectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
+    if (e.ctrlKey) {
+        // Top Selected or Not
+        let topSelected;
+        if (topCell) {
+            topSelected = topCell.hasClass("selected");
+        }
+
+        // bottom Selected or Not
+        let bottomSelected;
+        if (bottomCell) {
+            bottomSelected = bottomCell.hasClass("selected");
+        }
+
+        // left Selected or Not
+        let leftSelected;
+        if (leftCell) {
+            leftSelected = leftCell.hasClass("selected");
+        }
+
+        // right Selected or Not
+        let rightSelected;
+        if (rightCell) {
+            rightSelected = rightCell.hasClass("selected");
+        }
+
+
+        if (topSelected) {
+            $(ele).addClass("top-selected");
+            topCell.addClass("bottom-selected");
+        }
+
+        if (bottomSelected) {
+            $(ele).addClass("bottom-selected");
+            bottomCell.addClass("top-selected");
+        }
+
+        if (leftSelected) {
+            $(ele).addClass("left-selected");
+            leftCell.addClass("right-selected");
+        }
+
+        if (rightSelected) {
+            $(ele).addClass("right-selected");
+            rightCell.addClass("left-selected");
+        }
+
+    } else {
+        $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
+    }
+    $(ele).addClass("selected");
+}
