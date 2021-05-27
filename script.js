b2f7e3eff6096be1bab88728f7b11c0f9648489d
Vishal <vishaldutt157@gmail.com>
2021-05-27 22:42:37 +0530

    SetStye & Color Picker

--- script.js
+++ script.js
@@ -22,12 +22,28 @@ for (let i = 1; i <= 100; i++) {
     $("#rows").append(`<div class="row-name">${i}</div>`);
 }
 
+// Cell Data Default Properties
+let cellData = [];
+
 // Add Cells to the cell div
 for (let i = 1; i <= 100; i++) {
     let row = $(`<div class="cell-row"></div>`)
+    let rowArray = [];
     for (let j = 1; j <= 100; j++) {
         row.append(`<div id="row-${i}-col-${j}" class="input-cell" contenteditable="false"></div>`);
+        rowArray.push({
+            "font-family": "Noto Sans",
+            "font-size": 14,
+            "text": "",
+            "bold": false,
+            "italic": false,
+            "underlined": false,
+            "alignment": "left",
+            "color": "#444",
+            "bgcolor": "#fff"
+        });
     }
+    cellData.push(rowArray);
     $("#cells").append(row);
 }
 
@@ -53,6 +69,8 @@ $("#cells").scroll(function (e) {
 
 $(".input-cell").dblclick(function (e) {
     $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
+    // Add selected class to the selected cell
+    $(this).addClass("selected");
     $(this).attr("contenteditable", "true");
     $(this).focus();
 });
@@ -177,6 +195,35 @@ function selectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
         $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
     }
     $(ele).addClass("selected");
+    changeHeader(getRowCol(ele));
+}
+
+// This Function Removes the Selected class from the cell and add selected class 
+// to the last aligned cell
+// Two Way Alignment
+function changeHeader([rowId, colId]) {
+    let data = cellData[rowId - 1][colId - 1];
+    // console.log(data);
+    $(".alignment.selected").removeClass("selected");
+    $(`.alignment[data-type=${data.alignment}]`).addClass("selected");
+    // if(data.bold){
+    //     $("#bold").addClass("selected");
+    // }else {
+    //     $("#bold").removeClass("selected");
+    // }
+    addRemoveSelectFromFontStyle(data, "bold");
+    addRemoveSelectFromFontStyle(data, "italic");
+    addRemoveSelectFromFontStyle(data, "underlined");
+    $("#fill-color").css("border-bottom", `4px solid ${data.bgcolor}`);
+    $("#text-color").css("border-bottom", `4px solid ${data.color}`);
+}
+
+function addRemoveSelectFromFontStyle(data, property) {
+    if (data[property]) {
+        $(`#${property}`).addClass("selected");
+    } else {
+        $(`#${property}`).removeClass("selected");
+    }
 }
 
 // Select Cell Using Mouse Drag
@@ -205,6 +252,7 @@ function selectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
 // })
 
 // Select Cell using mouseenter function efficeint
+let count = 0;
 let startcellSelected = false;
 let startCell = {};
 let endCell = {};
@@ -349,3 +397,132 @@ $(".data-container").mouseup(function (e) {
     scrollXLStarted = false;
     scrollYBStarted = false;
 });
+
+// Hightlights text alignt icons ans alignt items when selected
+$(".alignment").click(function (e) {
+    let alignment = $(this).attr("data-type");
+    $(".alignment.selected").removeClass("selected");
+    $(this).addClass("selected");
+    $(".input-cell.selected").css("text-align", alignment);
+    // Traversing on cells For each loop in jquery
+    $(".input-cell.selected").each(function (index, data) {
+        // The data will print this
+        // console.log(data);
+        let [rowId, colId] = getRowCol(data);
+        cellData[rowId - 1][colId - 1].alignment = alignment;
+    });
+});
+
+// Change Text to bold
+$("#bold").click(function (e) {
+    setStyle(this, 'bold', "font-weight", "bold");
+});
+
+// Change Text to italic 
+$("#italic").click(function (e) {
+    setStyle(this, "italic", "font-style", "italic");
+});
+
+// Underline The Text
+$("#underlined").click(function (e) {
+    setStyle(this, "underlined", "text-decoration", "underline");
+});
+
+// Fucntion to change the style of the text
+function setStyle(ele, property, key, value) {
+    if ($(ele).hasClass("selected")) {
+        $(ele).removeClass("selected");
+        $(".input-cell.selected").css(key, "");
+        $(".input-cell.selected").each(function (index, data) {
+            let [rowId, colId] = getRowCol(data);
+            cellData[rowId - 1][colId - 1][property] = false;
+            //[property] act as a variable
+        });
+    } else {
+        $(ele).addClass("selected");
+        $(".input-cell.selected").css(key, value);
+        $(".input-cell.selected").each(function (index, data) {
+            let [rowId, colId] = getRowCol(data);
+            cellData[rowId - 1][colId - 1][property] = true;
+        });
+    }
+}
+
+//Color Picker
+// When we click on the colorpick icon the the .colorPick function get called two times 
+$(".pick-color").colorPick({
+    'initialColor': "#abcd",
+    'allowRecent': true,
+    'recentMax': 5,
+    'allowCustomColor': true,
+    'palette': ["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"],
+    'onColorSelected': function () {
+        //   this.element.css({'backgroundColor': this.color, 'color': this.color});
+        //   Defalut color will print #ABCD two times in console
+        //   console.log(this.color);
+        //   console.log(this.element);
+        //   this.element represents the jquery element
+        if (this.color != "#ABCD") {
+            // console.log(this.color);
+            // Current Element
+            // if we use [] on this then jquery wrapper removes automatically from this so we have to add wrapper by usng again $ on this
+            // this.element will give current div and this.element.children() will give children to the div
+            // console.log(this.element.children());
+            // $((this.element.children()[1]).attr("id"));
+            // Get id of the image selected 
+            // console.log($(this.element.children()[1]).attr("id"));
+            // this.element represents the div of the image
+            if ($(this.element.children()[1]).attr("id") == "fill-color") {
+                $(".input-cell.selected").css("background-color", this.color);
+                $("#fill-color").css("border-bottom", `4px solid ${this.color}`);
+                $(".input-cell.selected").each((index, data) => {
+                    let [rowId, colId] = getRowCol(data);
+                    cellData[rowId - 1][colId - 1].bgcolor = this.color;
+                });
+            }
+            if ($(this.element.children()[1]).attr("id") == "text-color") {
+                $(".input-cell.selected").css("color", this.color);
+                $("#text-color").css("border-bottom", `4px solid ${this.color}`);
+                $(".input-cell.selected").each((index, data) => {
+                    let [rowId, colId] = getRowCol(data);
+                    cellData[rowId - 1][colId - 1].color = this.color;
+                });
+            }
+        }
+    }
+});
+
+// If we click on the fill color the below code will click on the parent after 10sec i.e. click on the div
+$("#fill-color").click(function (e) {
+    setTimeout(() => {
+        $(this).parent().click();
+    }, 10)
+});
+
+$("#text-color").click(function (e) {
+    setTimeout(() => {
+        $(this).parent().click();
+    }, 10)
+});
+
+// Arrow fucntion Both this is same 
+// If there is no parent fucntion the the this will represent the this of the other parent ot this of point to the golbal this (window).
+// $("#text-color").click(function (e) {
+//     console.log(this);
+//     setTimeout(() => {
+//         // Below this will point to the outher this of the function
+//         // The value of outer this and inner this are same
+//         console.log(this);
+//         $(this).parent().click();
+//     }, 10)
+// });
+
+// Both this are dfferent
+// $("#text-color").click(function (e) {
+//     console.log(this);
+//     setTimeout(function () {
+//         console.log(this);
+//         // The below this will point to the setTimeout fucntion
+//         $(this).parent().click();
+//     }, 10)
+// });
